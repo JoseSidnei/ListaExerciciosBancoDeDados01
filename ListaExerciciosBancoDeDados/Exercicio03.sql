b4a192bcaa69a7affab8df5f045bb1bd73f14d19
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-06-21 17:24:35 -0300

    adicionado exercicios 1, 2 e 3

---
+++ ListaExerciciosBancoDeDados/Exercicio03.sql
@@ -0,0 +1,22 @@
+﻿
+CREATE TABLE champions(
+	nome			VARCHAR(100),
+	descricao		VARCHAR(100),
+	habilidade_1	VARCHAR(100),
+	habilidade_2	VARCHAR(100),
+	habilidade_3	VARCHAR(100),
+	habilidade_4	VARCHAR(100),
+	habilidade_5	VARCHAR(100)
+);
+
+INSERT INTO champions VALUES ('Katarina', 'a Lâmina Sinistra', 'Voracidade', 'Lâmina Saltitante', 'Preparação', 'Shunpo', 'Lótus da Morte');
+INSERT INTO champions VALUES ('Yasuo', 'o Imperdoável', 'Estilo do Errante', 'Tempestade de Aço', 'Parede de Vento', 'Espada Ágil', 'Último Suspiro');
+INSERT INTO champions(nome, descricao, habilidade_1)
+ VALUES ('Master Yi', ' o Espadachim Wuju', 'Ataque Duplo');
+ INSERT INTO champions VALUES ('Vayne', 'a Caçadora Noturna', 'Caçadora Noturna', 'Rolamento', 'Dardos de Prata', 'Condenar', 'Hora Final');
+ INSERT INTO champions VALUES ('Lee Sin', 'o Monge Cego', 'Agitação', 'Onda Sônica/Ataque Ressonante', 'Proteger/Vontade de ferro', 'Tempestade/Multilar', 'Furia do Dragão');
+ INSERT INTO champions VALUES ('Vi','a Defensora de Piltover','Blindagem', 'Quebra-Cofres', 'Pancada Certeira', 'Força Excessiva', 'Saque e Enterrada');
+INSERT INTO champions VALUES ('Diana','o Escárnio da Lua','Espada de Prata Lunar', 'Golpe Crescente', 'Cascata Lívida', 'Colapso Minguante', 'Zênite Lunar');
+INSERT INTO champions VALUES ('Annie','a Criança Sombria','Piromania', 'Desintegrar', 'Incinerar', 'Escudo Fundido', 'Invocar: Tibbers');
+INSERT INTO champions VALUES ('Aatrox','a Espada Darkin','Poço de Sangue', 'Voo Sombrio', 'Sede de Sangue / Preço em Sangue', 'Lâminas da Aflição', 'Massacre');
+
